bd10dbe95d3f57640eec5d943c84270167d31218
Vuepress Autobuild in docker <vuepress@docker.build>
2019-07-22 21:45:52 +0800

    Auto Deploy

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,6 @@
 set -e
 git config --global user.email "vuepress@docker.build"
 git config --global user.name "Vuepress Autobuild in docker"
-git pull origin master
 git add -A
 git commit -m 'Auto Deploy'
 git push origin master
