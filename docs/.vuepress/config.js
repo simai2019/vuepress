8ccf6d176a4ea2bbf4a1b00fa51bf7f4bd3844bd
Vuepress Autobuild in docker <vuepress@docker.build>
2019-07-22 21:45:17 +0800

    ok

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -2,7 +2,6 @@ module.exports = {
   themeConfig: {
     nav: [
       { text: '首页', link: '/' },
-      { text: '最近更新', link: '/recently/' },
       { text: '信仰探索', link: '/#信仰探索' },
       { text: '生活疑难', link: '/#生活疑难' },
       { text: '思麦与我', link: '/#思麦与我' },
