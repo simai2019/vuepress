e7e10da6e4b42a83d07d48fff88786ab700ab266
Vuepress Autobuild in docker <vuepress@docker.build>
2019-10-01 06:37:33 +0800

    Auto Deploy

--- docs/index.js
+++ docs/index.js
@@ -19,7 +19,7 @@ function getDirectoriesRecursive(srcpath) {
 
 
 
-var folders = ['生活疑难','信仰探索','思麦与我','latest']
+var folders = ['生活疑难','信仰探索','思麦与我','latest','updated']
 
 folders.forEach(function(folder){
 
